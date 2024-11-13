36a031db7ddc009b3bb69dd50b4ce55766b9826a
aimansyakir15 <aiman.zulkifli10@gmail.com>
2024-11-13 15:14:10 +0800

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full bg-[#161513] py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-white text-sm flex items-center">
           {new Date().getFullYear()} © Aiman Syakir.
         </p>
